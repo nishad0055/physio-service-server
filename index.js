e0419a886ef0734ade1fc66a49c248d7c23f46a6
nishadahmed5 <nishatahmed650@gmail.com>
2022-11-10 22:15:02 +0600

    last commit

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ app.get('/', (req, res)=>{
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tbf6iah.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+ const verifyJWT = (req, res, next) =>{
+ 
+   const authHeader = req.headers.authorization;
+   if(!authHeader){
+    res.status(401).send({message:'Unauthorized access'})
+   }
+   const token = authHeader.split(' ')[1]
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      res.status(401).send({message: 'Invalid User'})
+    }
+    req.decoded = decoded;
+    next()
+   })
+ } 
+
 async function run(){
     try{
        const productCollection = client.db('serviceData').collection('services')
@@ -29,12 +45,17 @@ async function run(){
        app.post('/jwt', (req, res)=>{
         
         const user = req.body
-        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
         res.send({token})
        })
 
-       app.get('/reviews', async(req, res)=>{
-          
+       app.get('/reviews',verifyJWT,  async(req, res)=>{
+          const decoded = req.decoded;
+          console.log('inside review page', decoded)
+          if(decoded.email !== req.query.email){
+              res.status(403).send({message: 'unauthorized access'})
+          }
+        
          let query = {}
          if(req.query.email){
           query = {
